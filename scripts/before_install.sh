b82ec108ddb09c59177c2066d6f0391dfe7155dc
faizuuuuuu <s222628865@deakin.edu.au>
2024-09-26 03:29:26 +1000

    first commit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Clean up any previous deployments
+echo "Cleaning up previous deployment..."
 sudo rm -rf /var/www/html/*
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Navigate to the application directory
 cd /var/www/html
+
+# Install dependencies
 npm install
